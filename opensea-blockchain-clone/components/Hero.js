5e76c0d23d217bd7f527615ffef998cdf47a6221
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-13 16:27:06 +0200

    $0013: [HomePage]: add nft info container with author and link to asset; spacing

--- opensea-blockchain-clone/components/Hero.js
+++ opensea-blockchain-clone/components/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const style = {
     wrapper: `relative`,
@@ -15,33 +15,52 @@ const style = {
     author: `flex flex-col justify-center ml-4`,
     name: ``,
     infoIcon: `flex justify-end items-center flex-1 text-[#8a939b] text-3xl font-bold`,
-  }
+}
 
 const Hero = () => {
-    return <div className={style.wrapper}>
-        <div className={style.container}>
-            <div className={style.contentWrapper}>
-                <div className={style.copyContainer}>
-                    <div className={style.title}>
-                        Discover, collect and sell extraordinary NFTs
-                    </div>
-                    <div className={style.description}>
-                            OpenSea is the world&apos;s first and largest NFT marketplace
-                    </div>
-                    <div className={style.ctaContainer}>
-                        <button className={style.accentedButton}>Explore</button>
-                        <button className={style.button}>Create</button>
-                    </div>
-                </div>
-                <div className={style.cardContainer}>
-                    <img className="rounded-t-lg"
-                        src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
-                        alt=""
-                    />
-                </div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.contentWrapper}>
+          <div className={style.copyContainer}>
+            <div className={style.title}>
+              Discover, collect, and sell extraordinary NFTs
             </div>
-        </div>    
+            <div className={style.description}>
+              OpenSea is the world&apos;s first and largest NFT marketplace
+            </div>
+            <div className={style.ctaContainer}>
+              <button className={style.accentedButton}>Explore</button>
+              <button className={style.button}>Create</button>
+            </div>
+          </div>
+          <div className={style.cardContainer}>
+            <img
+              className="rounded-t-lg"
+              src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
+              alt=""
+            />
+            <div className={style.infoContainer}>
+              <img
+                className="h-[2.25rem] rounded-full"
+                src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
+                alt=""
+              />
+              <div className={style.author}>
+                <div className={style.name}>Jolly</div>
+                <a
+                  className="text-[#1868b7]"
+                  href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/2324922113504035910649522729980423429926362207300810036887725141691069366277"
+                >
+                  hola-kanola
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-};
+  )
+}
 
-export default Hero;
+export default Hero
