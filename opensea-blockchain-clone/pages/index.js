4006b4ba72e09a701db5a33a02f35548019363f1
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-13 15:59:05 +0200

    $0010: [HomePage]: add hero.js for home page

--- opensea-blockchain-clone/pages/index.js
+++ opensea-blockchain-clone/pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home(){
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+  
 }
