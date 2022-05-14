42afcacc386edd6e40508c54b6daba0b1fb10290
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-14 12:13:03 +0200

    $0017: [Metamask]: add style for wallet connect wrapper

--- opensea-blockchain-clone/pages/index.js
+++ opensea-blockchain-clone/pages/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
+
 export default function Home(){
   return (
     <>
