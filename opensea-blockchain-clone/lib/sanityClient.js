c50a8b391507b3246d3086623b1677ef4170030a
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-14 16:25:29 +0200

    Revert "$0022: [Metamask]: [Sanity]: update sanity api version"
    This reverts commit a7f09f8df5b12cab87d3e906816ddb1a052263d4.

--- opensea-blockchain-clone/lib/sanityClient.js
+++ opensea-blockchain-clone/lib/sanityClient.js
@@ -3,7 +3,7 @@ import sanityClient from '@sanity/client'
 export const client = sanityClient({
   projectId: 'xrdcge3m',
   dataset: 'production',
-  apiVersion: '2021-03-25',
+  apiVersion: '2022-05-14',
   token:
     'skAz3dmDMTZ6pp5ry203nYNXEWxQZR2KytHYAzfBHOss3xKovGVKCfSRoKGbXYuqdXsaX928dvu0TWd0PgxOPZhVD8D84t1OvrVd1yp1oNNH9ImCSHU95l6VOtbuiSXzMWIq5R3mHnmsDYOUb5BfNfvAy0kcrEW1cPhZ8XSdk9aml9BUZfzQ',
   useCdn: false,
