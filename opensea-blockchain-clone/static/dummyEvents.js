90b2ea77640ec370bf8beb5c7ac137f52b231b6d
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-15 23:08:31 +0200

    $0045: [NFTDetailsPage] [ItemActivity]: fix typo

--- opensea-blockchain-clone/static/dummyEvents.js
+++ opensea-blockchain-clone/static/dummyEvents.js
@@ -4,28 +4,28 @@ export const dummyEvents = [
         price: 7777,
         from: '0xB3N',
         to: '0xG3P',
-        date: '3 months ago'
+        date: '3 months ago',
     },
 
     {
         price: 777,
         from: '0xT1M',
         to: '0xB3N',
-        date: '6 months ago'
+        date: '6 months ago',
     },
 
     {
         price: 77,
         from: '0xM4K',
         to: '0xT1M',
-        date: '7 months ago'
+        date: '7 months ago',
     },
 
     {
         price: 7,
         from: '0xG33K',
         to: '0xM4K',
-        date: '8 months ago'
+        date: '8 months ago',
     },
 
 ]
